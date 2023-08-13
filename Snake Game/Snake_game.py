6ae8bb29ab0a6ae9770fb67116afdf42bc9a1d4e
Sondre Indset <sondre2004@hotmail.no>
2023-08-13 16:43:45 +0200

    Update Snake_game.py

--- Snake Game/Snake_game.py
+++ Snake Game/Snake_game.py
@@ -1,18 +1,19 @@
+
 import pygame, random
 
-# Definer farger og skjermstørrelse
+# Definerer farger og skjermstørrelse
 SKJERM_STØRRELSER = [720, 480]
 bakgrunnsfarge = (0, 0, 0)  # Svart
 snakefarge = (0, 255, 0)  # grønn
 matfarge = (255, 255, 255)  # Hvit
 
-# Definer konstanter for spillet
+# Definer verdier til slangen og maten
 BREDDE = 20
 HOYDE = 20
 SNAKE_HASTIGHET = 15
 SNAKE_MAT_INCREMENT = 1
 
-# Sett opp Pygame
+# Setter opp Pygame
 pygame.init()
 skjerm = pygame.display.set_mode((SKJERM_STØRRELSER[0], SKJERM_STØRRELSER[1]))
 pygame.display.set_caption("Snake Game by Sondre Indset")
@@ -27,6 +28,8 @@ class Snake:
         self.mat_spist = 0
 
     def bevegelse(self):
+        """ Metoden gjør at slangen kan bevege seg, og øker i størrelse hvis den spiser mat """
+        # Kopier posisjonen til hodet
         ny_hode_pos = self.snake_kropps_deler[0].copy()
 
         # Flytt slangen i riktig retning
@@ -43,30 +46,30 @@ class Snake:
             ny_hode_pos[1] += SNAKE_HASTIGHET
             self.y += SNAKE_HASTIGHET
 
-        # Legg til ny hodeposisjon i starten av listen
+        # Legger til ny hodeposisjon i starten av listen
         self.snake_kropps_deler.insert(0, ny_hode_pos)
 
-        # Hvis slangen har spist mat, øk mat_spist og la være å fjerne siste kroppsdel
+        # Hvis slangen ikek spiser mat fjernes siste kroppsdelen
         if self.mat_spist > 0:
             self.mat_spist -= 1
         else:
-            # Fjern siste kroppsdel
             self.snake_kropps_deler.pop()
 
     def tegn_snake(self):
-        # Tegn slangekroppen
+        """ Metoden tegner slangen med hodet og alle kroppsdelene """
         for delen in self.snake_kropps_deler:
             pygame.draw.rect(skjerm, snakefarge, [delen[0], delen[1], BREDDE, HOYDE])
 
 # Klasse for maten
 class Mat:
     def __init__(self):
-        self.x = random.randrange(10, (SKJERM_STØRRELSER[0] - BREDDE) - 10, 10)
-        self.y = random.randrange(10, (SKJERM_STØRRELSER[1] - HOYDE) - 10, 10)
+        self.x = random.randrange(0, (SKJERM_STØRRELSER[0] // BREDDE)) * BREDDE
+        self.y = random.randrange(0, (SKJERM_STØRRELSER[1] // HOYDE)) * HOYDE
 
     def tegn_mat(self):
-        # Tegn maten på skjermen
+        """ Metoden tegner maten på skjermen med en tilfeldig posisjon"""
         pygame.draw.rect(skjerm, matfarge, [self.x, self.y, BREDDE, HOYDE])
+
 
 # Oppretter slangen og maten
 snake = Snake()
@@ -95,18 +98,17 @@ while not game_over:
                 snake.retning = "OPP"
             elif event.key == pygame.K_DOWN and snake.retning != "OPP":
                 snake.retning = "NED"
-    
-    # Tegn alt på skjermen
-    skjerm.fill(bakgrunnsfarge)
 
+    # Tegner alt på skjermen
+    skjerm.fill(bakgrunnsfarge)
     snake.tegn_snake()
     mat.tegn_mat()
 
-    # Beveg slangen
+    # Beveger slangen
     snake.bevegelse()
 
-    # Sjekk om slangen spiser maten
-    spiste_mat = (snake.x-10 <= mat.x <= snake.x+10) and (snake.y-10 <= mat.y <= snake.y+10)
+    # Sjekk om slangen spiser maten, hvis slangen spiser mat øker også hastigheten
+    spiste_mat = (snake.x - BREDDE // 2 <= mat.x <= snake.x + BREDDE // 2) and (snake.y - HOYDE // 2 <= mat.y <= snake.y + HOYDE // 2)
 
     if spiste_mat:
         mat = Mat()
@@ -114,13 +116,15 @@ while not game_over:
         SNAKE_HASTIGHET += 0.5
         snake.mat_spist += 1
         
-    # Sjekk om slangen treffer veggen eller seg selv
-    if snake.x < 0 or snake.x > SKJERM_STØRRELSER[0] - BREDDE or snake.y < 0 or snake.y > SKJERM_STØRRELSER[1] - HOYDE:
+    # Sjekk om hodet til slangen treffer en av veggene eller seg selv
+    if (snake.x + BREDDE // 2 <= 0) or (snake.x >= SKJERM_STØRRELSER[0] - BREDDE // 2) or (snake.y <= 0) or (snake.y >= SKJERM_STØRRELSER[1] - HOYDE):
         game_over = True
     for delen in snake.snake_kropps_deler[1:]:
         if delen == [snake.x, snake.y]:
             game_over = True
+            break
 
+    # Viser poengscoren
     poengtekst = font.render("Score: " + str(score), True, (255, 255, 255))
     skjerm.blit(poengtekst, (5, 5))
 
